b41cc38fe25007de6f28817d863f46baf6bf08f0
vfpark <vifrankpark@gmail.com>
2023-02-07 12:38:12 -0300

    HelloWord

---
+++ helloword/src/helloword/HelloWord.java
@@ -0,0 +1,12 @@
+package helloword;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Word!");
+		System.out.println("Turma 61 é top!");
+	
+	}
+
+}
